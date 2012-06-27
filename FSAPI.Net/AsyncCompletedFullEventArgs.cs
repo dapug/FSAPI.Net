09bcbbf5ef025a89a0087a2d5fdc531a1f6176bf
dapug <roachslayer@hotmail.com>
2012-06-27 16:39:01 -0700

    Added licensing

--- FSAPI.Net/AsyncCompletedFullEventArgs.cs
+++ FSAPI.Net/AsyncCompletedFullEventArgs.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------
+// <copyright file="AsyncCompletedFullEventArgs.cs" company="nGenesis, LLC">
+//     Copyright (c) 2012, nGenesis, LLC. 
+//     All rights reserved. This program and the accompanying materials 
+//     are made available under the terms of the Eclipse Public License v1.0 
+//     which accompanies this distribution (Liscense.htm), and is available at 
+//     http://www.eclipse.org/legal/epl-v10.html 
+//     
+//     Contributors: 
+//         dapug - Initial author, core functionality
+// </copyright>
+//
+// ------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
