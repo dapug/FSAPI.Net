58a3cbcf2277e66409c90958f7da29a45bca80cc
David <dapug@fs>
2012-06-27 17:55:19 -0700

    test2

--- FSAPI.Net/Identity/Basic.cs
+++ FSAPI.Net/Identity/Basic.cs
@@ -33,7 +33,6 @@ namespace FSAPI.Identity
         public bool Login(string username, string password, string devKey)
         {
             return false;
-
         }
 
         public void LoginAsync(string username, string password, string devKey)
